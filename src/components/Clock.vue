c660dc828fbf6bfca8048171513aea7694062dc5
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2020-03-31 22:53:13 +0200

    Touch Trial

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -63,6 +63,38 @@ for (let h = 0; h < 12; h++) {
   }
 }
 
+(function() {
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	if (window.AudioContext) {
+		window.audioContext = new window.AudioContext();
+	}
+	var fixAudioContext = function (e) {
+		if (window.audioContext) {
+			// Create empty buffer
+			var buffer = window.audioContext.createBuffer(1, 1, 22050);
+			var source = window.audioContext.createBufferSource();
+			source.buffer = buffer;
+			// Connect to output (speakers)
+			source.connect(window.audioContext.destination);
+			// Play sound
+			if (source.start) {
+				source.start(0);
+			} else if (source.play) {
+				source.play(0);
+			} else if (source.noteOn) {
+				source.noteOn(0);
+			}
+		}
+		// Remove events
+		this.$refs.clock.removeEventListener('touchstart', fixAudioContext);
+		this.$refs.clock.removeEventListener('touchend', fixAudioContext);
+	};
+	// iOS 6-8
+	this.$refs.clock.addEventListener('touchstart', fixAudioContext);
+	// iOS 9
+	this.$refs.clock.addEventListener('touchend', fixAudioContext);
+})();
+
 export default {
   name: "Clock",
   props: {
