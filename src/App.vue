033680744d67a18b7433824cb47883223231cbd6
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2020-04-29 11:01:31 +0200

    Add homepage and navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
+    <NavBar v-if="this.$router.currentRoute.path != '/'"/>
     <router-view/>
   </div>
 </template>
 
 <script>
   import {defaultCache} from "./common/AudioCache";
+  import NavBar from './components/shared/NavBar'
 
   export default {
     name: "App",
-    components: {},
+    components: {NavBar},
     mounted() {
       defaultCache.bootstrapElement(document.body);
+      console.log("route", this.$router.currentRoute)
     }
   };
 </script>
 
 <style>
+body{
+  margin: 0px;
+}
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
   }
 </style>
