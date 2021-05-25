3dfecc95fea0ca425f0a58f310362697e9709ebf
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2021-05-25 19:43:53 +0200

    added multiplication tables

---
+++ src/components/Success.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="success">
+        <h1>Tusen takk for tilbakemeldingen!</h1>
+        <h2></h2>
+        <p>Tilbake til <router-link to="/">forsiden</router-link>.</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.success{
+    opacity: 0;
+    transform: scale(0.5);
+    animation-name: fadeIn;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+/*     animation: fadeIn 2s ease-in 5s;  */
+}
+
+@keyframes fadeIn {
+    from {opacity: 0;
+    transform: scale(0.5)}
+    to {opacity: 1;
+    transform: scale(1)}
+}
+</style>
--- src/components/memo/sets.js
+++ src/components/memo/sets.js
@@ -46,10 +46,45 @@ export let sets = [
     },
     {
         type: SetTypeEnum.DIFFERENT,
+        name: '4-gangen',
+        cardsInPair: 2,
+        category: SetCategoryEnum.MATTE,
+        values: [["1 x 4", "4"], ["2 x 4", "8"], ["3 x 4", "12"], ["4 x 4", "16"], ["5 x 4", "20"], ["6 x 4", "24"], ["7 x 4", "28"], ["8 x 4", "32"], ["9 x 4", "36"], ["10 x 4", "40"]]
+    },
+    {
+        type: SetTypeEnum.DIFFERENT,
         name: '5-gangen',
         cardsInPair: 2,
         category: SetCategoryEnum.MATTE,
         values: [["1 x 5", "5"], ["2 x 5", "10"], ["3 x 5", "15"], ["4 x 5", "20"], ["5 x 5", "25"], ["6 x 5", "30"], ["7 x 5", "35"], ["8 x 5", "40"], ["9 x 5", "45"], ["10 x 5", "50"]]
+    },
+    {
+        type: SetTypeEnum.DIFFERENT,
+        name: '6-gangen',
+        cardsInPair: 2,
+        category: SetCategoryEnum.MATTE,
+        values: [["1 x 6", "6"], ["2 x 6", "12"], ["3 x 6", "18"], ["4 x 6", "24"], ["5 x 6", "30"], ["6 x 6", "36"], ["7 x 6", "42"], ["8 x 6", "48"], ["9 x 6", "54"], ["10 x 6", "60"]]
+    },
+    {
+        type: SetTypeEnum.DIFFERENT,
+        name: '7-gangen',
+        cardsInPair: 2,
+        category: SetCategoryEnum.MATTE,
+        values: [["1 x 7", "7"], ["2 x 7", "14"], ["3 x 7", "21"], ["4 x 7", "28"], ["5 x 7", "35"], ["6 x 7", "42"], ["7 x 7", "49"], ["8 x 7", "56"], ["9 x 7", "63"], ["10 x 7", "70"]]
+    },
+    {
+        type: SetTypeEnum.DIFFERENT,
+        name: '8-gangen',
+        cardsInPair: 2,
+        category: SetCategoryEnum.MATTE,
+        values: [["1 x 8", "8"], ["2 x 8", "16"], ["3 x 8", "24"], ["4 x 8", "32"], ["5 x 8", "40"], ["6 x 8", "48"], ["7 x 8", "56"], ["8 x 8", "64"], ["9 x 8", "72"], ["10 x 8", "80"]]
+    },
+    {
+        type: SetTypeEnum.DIFFERENT,
+        name: '9-gangen',
+        cardsInPair: 2,
+        category: SetCategoryEnum.MATTE,
+        values: [["1 x 9", "9"], ["2 x 9", "18"], ["3 x 9", "27"], ["4 x 9", "36"], ["5 x 9", "45"], ["6 x 9", "54"], ["7 x 9", "63"], ["8 x 9", "72"], ["9 x 9", "81"], ["10 x 9", "9g0"]]
     },
     {
         type: SetTypeEnum.DIFFERENT,
